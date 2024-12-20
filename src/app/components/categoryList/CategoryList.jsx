d3d7422486743eaecdd8f7b161a94bcd2d87db79
floriansaliba <florian.saliba@gmail.com>
2024-12-20 13:13:59 +0100

    Add CategoryList componant

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -9,86 +9,84 @@ const CategoryList = () => {
       <h1 className={styles.title}>Catégories populaires</h1>
       <div className={styles.categories}>
         <div className={styles.categories}>
-          <div className={styles.category}>
-            <Link
-              href='/blog?cat=style'
-              className={`${styles.category} ${styles.style}`}
-            >
-              <Image
-                src='/style.png'
-                alt=''
-                width={32}
-                height={32}
-                className={styles.image}
-              ></Image>
-              style
-            </Link>
-            <Link
-              href='/blog?cat=style'
-              className={`${styles.category} ${styles.fashion}`}
-            >
-              <Image
-                src='/style.png'
-                alt=''
-                width={32}
-                height={32}
-                className={styles.image}
-              ></Image>
-              fashion
-            </Link>
-            <Link
-              href='/blog?cat=style'
-              className={`${styles.category} ${styles.food}`}
-            >
-              <Image
-                src='/style.png'
-                alt=''
-                width={32}
-                height={32}
-                className={styles.image}
-              ></Image>
-              food
-            </Link>
-            <Link
-              href='/blog?cat=style'
-              className={`${styles.category} ${styles.culture}`}
-            >
-              <Image
-                src='/style.png'
-                alt=''
-                width={32}
-                height={32}
-                className={styles.image}
-              ></Image>
-              culture
-            </Link>
-            <Link
-              href='/blog?cat=style'
-              className={`${styles.category} ${styles.travel}`}
-            >
-              <Image
-                src='/style.png'
-                alt=''
-                width={32}
-                height={32}
-                className={styles.image}
-              ></Image>
-              travel
-            </Link>
-            <Link
-              href='/blog?cat=style'
-              className={`${styles.category} ${styles.food}`}
-            >
-              <Image
-                src='/style.png'
-                alt=''
-                width={32}
-                height={32}
-                className={styles.image}
-              ></Image>
-              food
-            </Link>
-          </div>
+          <Link
+            href='/blog?cat=style'
+            className={`${styles.category} ${styles.style}`}
+          >
+            <Image
+              src='/style.png'
+              alt=''
+              width={32}
+              height={32}
+              className={styles.image}
+            ></Image>
+            style
+          </Link>
+          <Link
+            href='/blog?cat=style'
+            className={`${styles.category} ${styles.fashion}`}
+          >
+            <Image
+              src='/fashion.png'
+              alt=''
+              width={32}
+              height={32}
+              className={styles.image}
+            ></Image>
+            fashion
+          </Link>
+          <Link
+            href='/blog?cat=style'
+            className={`${styles.category} ${styles.food}`}
+          >
+            <Image
+              src='/food.png'
+              alt=''
+              width={32}
+              height={32}
+              className={styles.image}
+            ></Image>
+            food
+          </Link>
+          <Link
+            href='/blog?cat=style'
+            className={`${styles.category} ${styles.culture}`}
+          >
+            <Image
+              src='/culture.png'
+              alt=''
+              width={32}
+              height={32}
+              className={styles.image}
+            ></Image>
+            culture
+          </Link>
+          <Link
+            href='/blog?cat=style'
+            className={`${styles.category} ${styles.travel}`}
+          >
+            <Image
+              src='/travel.png'
+              alt=''
+              width={32}
+              height={32}
+              className={styles.image}
+            ></Image>
+            travel
+          </Link>
+          <Link
+            href='/blog?cat=style'
+            className={`${styles.category} ${styles.food}`}
+          >
+            <Image
+              src='/food.png'
+              alt=''
+              width={32}
+              height={32}
+              className={styles.image}
+            ></Image>
+            food
+          </Link>
         </div>
       </div>
     </div>
