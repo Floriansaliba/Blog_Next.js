9fd3d80fb81758b1b98b3560cb59410ad9fb8144
floriansaliba <florian.saliba@gmail.com>
2024-12-22 15:06:34 +0100

    Add Card component

---
+++ src/app/components/card/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './card.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src='/p1.jpeg' alt='' fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2023 - </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          quos quibusdam aspernatur nulla cum, ipsum quis. Animi quibusdam fuga
+          cum temporibus eius, harum corrupti enim quis aut iure sit itaque?{' '}
+        </p>
+        <Link href='/' className={styles.link}>
+          Lire plus
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
